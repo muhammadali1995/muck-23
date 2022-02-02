6e921613377975bb9c6ea236f521de00d74284b6
Zohidjon-Toshpulatov <zohid99jon@gmail.com>
2022-02-02 16:30:39 +0500

    Working on home page and navbar

---
+++ src/components/HomeAutomateTrade.jsx
@@ -0,0 +1,18 @@
+export const HomeAutomateTrade = () => {
+    return <>
+        <div className="w-100  text-center bg-cover bg-no-repeat  bg-home-automate-trade rounded-[20px]">
+            <div className="w-full bg-[#4f4f4f] bg-opacity-70 pt-32 pb-[100px] rounded-[20px]">
+                <h1 className="uppercase font-semibold text-[34px] leading-[45px] text-white">
+                    Automate your trading and take
+                    <span className='block font-light'> emotion out of the equation</span>
+                </h1>
+                <p className="text-lg leading-7 text-white mt-3 mb-[38px]">
+                    Invest in all cryptocurrencies your exchange offers. <br />
+                    Gain access to an expert suite of tools like our trailing <br />
+                    features that help you buy/sell better than before
+                </p>
+                <button className="header-btn-try">try it now</button>
+            </div>
+        </div>
+    </>;
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,35 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      blue: {
-        light: '#0089D31A'
+
+    extend: {
+      backgroundImage: {
+        'home-automate-trade': 'url("./assets/home-1.png")',
+        'home-copy-trade': "url('./assets/home-2.png')"
+      },
+      colors: {
+        'accent': '#00AECC',
+        'primary': '#045DB6',
+        'secondary': 'rgba(0, 137, 211, 0.1)',
+        'color-text': '#333333',
+        'dark-primary': '#022F5B',
+      },
+      fontSize: {
+        'header-title': '82px',
+      },
+      lineHeight: {
+        'header-title': '88px',
+        'header-btn-try': '30px',
+      },
+      screens: {
+        '3xl': '1920px',
+      },
+      spacing: {
+        '5.5': '22px',
+        '17.5': '70px',
+        75: '300px',
       }
     },
-    maxWidth: {
-      desktop: '1000px'
-    },
-    extend: {
-
-    },
   },
-  plugins: [],
+  plugins: [require('@themesberg/flowbite/plugin')],
 }
