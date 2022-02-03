f7a2b4545cb74f8260ef2c39d19bb16d4ed3e5c3
unknown <diyorbekabdumannonov4@gmail.com>
2022-02-03 16:07:16 +0500

    small change

--- src/pages/MirrorTrading.jsx
+++ src/pages/MirrorTrading.jsx
@@ -1,11 +1,13 @@
 import MirrorTraderMain from './../components/MirrorTraderMain'
 import Currencies from './../components/Currencies'
+import Steps from './../components/Steps'
 
 export default function MirrorTrading() {
     return (
         <>
             <MirrorTraderMain />
             <Currencies />
+            <Steps/>
         </>
     )
 }
