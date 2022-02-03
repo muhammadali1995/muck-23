397b2d77767cfe269e117931309b5b2afbc6250b
unknown <diyorbekabdumannonov4@gmail.com>
2022-02-03 14:36:28 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,14 @@ import cloudflare from './../assets/cloudflare.png'
 
 export default function Footer() {
     return (
-        <div className="bg-dark-primary max-w-desktop mx-auto py-8 text-white">
+        <div className="bg-dark-primary max-w-desktop mx-auto py-20 text-white">
             <div className="flex items-center md:items-start justify-between flex-col-reverse lg:flex-row mx-auto max-w-[1000px]">
-                <div className="py-20 items-center lg:items-start flex flex-col-reverse lg:flex-col w-full lg:w-1/3">
+                <div className="items-center lg:items-start flex flex-col-reverse lg:flex-col w-full lg:w-1/3">
+                    <button className='text-[24px] hidden lg:block  font-normal text-white bg-accent py-[23px] px-[45px]'>LOGO</button>
+                    <p className='hidden lg:block font-normal mt-[30px] text-sm mb-[25px]'>The information on this website is general in nature. Please consider the information in light of your objectives, financial situation and needs.
+                        <br /><br />
+                        No Name 1 is a trademark of No name 2
+                    </p>
                     <div className='flex flex-col lg:flex-col'>
                         <h3 className="font-medium mb-4 text-[22px]">Website secured with:</h3>
                         <div className="flex items-center">
@@ -13,9 +18,9 @@ export default function Footer() {
                             <img src={cloudflare} alt="cloudflare" />
                         </div>
                     </div>
-                    <button className="bg-accent py-4 px-6 my-16 w-64 md:w-56">MARKETPLACE</button>
+                    <button className="bg-accent py-4 px-6 mb-16 mt-8 md:mt-16 w-64 md:w-56">MARKETPLACE</button>
                 </div>
-                <div className="flex ml-0 md:ml-16 items-center md:items-start text-center md:text-left justify-center sm:justify-between flex-col md:flex-row w-full md:w-8/12 flex-wrap">
+                <div className="flex ml-0 md:ml-16 items-center md:items-start text-center md:text-left justify-center sm:justify-between flex-col md:flex-row w-full lg:w-8/12 pr-0 md:pr-20 lg:pr-0 flex-wrap">
                     <ul className="uppercase text-xl my-12 md:my-0">
                         <li className="cursor-pointer">Services</li>
                         <li className="cursor-pointer mt-2">Solutions</li><li>MarketPlace</li>
@@ -32,7 +37,7 @@ export default function Footer() {
                         <li className="cursor-pointer mt-2">Pricings</li>
                         <li className="cursor-pointer mt-2">HelpDesk</li>
                     </ul>
-                    <ul className="uppercase text-lg my-12 md:my-0">
+                    <ul className="uppercase text-lg mb-0 md:mb-12 mt-12 md:my-0">
                         <li className="cursor-pointer">Privacy Policy</li>
                         <li className="cursor-pointer mt-2">Terms and Conditions</li>
                         <li className="cursor-pointer mt-2">Support</li>
