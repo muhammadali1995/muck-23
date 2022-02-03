c98c57524656d0cb791630981d29e19df044ba55
unknown <diyorbekabdumannonov4@gmail.com>
2022-02-03 17:38:09 +0500

    Update Solution.jsx

--- src/pages/Solution.jsx
+++ src/pages/Solution.jsx
@@ -2,12 +2,16 @@ import SolutionMain from "../components/SolutionMain";
 import Marketplace from './../components/Marketplace'
 import Footer from './../components/Footer'
 import SolutionSteps from "../components/SolutionSteps";
+import Navbar from './../components/Navbar'
 
 export default function Solution() {
     return (
         <>
+            <div className="bg-[#0089D31A]">
+                <Navbar />
+            </div>
             <SolutionMain />
-            <SolutionSteps/>
+            <SolutionSteps />
             <Marketplace />
             <div className='mt-20'>
                 <Footer />
