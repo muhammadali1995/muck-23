95771c5bacb885c79ec8d8db84b79247fbed28f9
unknown <diyorbekabdumannonov4@gmail.com>
2022-02-03 19:18:49 +0500

    Added navbars

--- src/components/SolutionMain.jsx
+++ src/components/SolutionMain.jsx
@@ -1,24 +1,36 @@
 import racket from './../assets/racket.png'
 import arrow from './../assets/arrow.png'
+import Navbar from './../components/Navbar'
+import Registration from './../components/Registration'
+
 
 export default function SolutionMain() {
     return (
-        <div className="bg-[#0089D31A] pt-20 pb-40">
-            <div className="mx-auto max-w-[1200px] w-full flex items-center flex-col p-8 lg:p-0 lg:flex-row justify-between">
-                <div className="w-full mr-6 m-4">
-                    <h1 className="font-extrabold text-2xl md:text-6xl"><span className='text-accent block'> Jumpstart </span>
-                        your trading.</h1>
-                    <div className="font-medium mt-10 text-base w-full flex items-center">
-                        <p>Copy. Acticate </p>
-                        <img src={arrow} alt="arrow" className='mx-4' />
-                        <p>Jumpstart.</p>
-                    </div>
-                    <button className="mt-6 py-4 px-8 text-white font-medium bg-accent">TRY IT NOW</button>
+        <div className="bg-secondary">
+            <div className=" max-w-[1200px] mx-auto">
+                <div className="py-[50px]">
+                    <Registration color={'text-color-text'} />
                 </div>
-                <div>
-                    <img alt="robots" src={racket} />
+                <Navbar navColor={'text-color-text'} />
+                <div className="pt-20 pb-40">
+                    <div className="mx-auto  w-full flex items-center flex-col p-8 lg:p-0 lg:flex-row justify-between">
+                        <div className="w-full mr-6 m-4">
+                            <h1 className="font-extrabold text-2xl md:text-6xl"><span className='text-accent block'> Jumpstart </span>
+                                your trading.</h1>
+                            <div className="font-medium mt-10 text-base w-full flex items-center">
+                                <p>Copy. Acticate </p>
+                                <img src={arrow} alt="arrow" className='mx-4' />
+                                <p>Jumpstart.</p>
+                            </div>
+                            <button className="mt-6 py-4 px-8 text-white font-medium bg-accent">TRY IT NOW</button>
+                        </div>
+                        <div>
+                            <img alt="robots" src={racket} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
--- src/components/WebTraderMain.jsx
+++ src/components/WebTraderMain.jsx
@@ -1,18 +1,27 @@
 import laptop from './../assets/WebTrader_Laptop_design.png'
+import Registration from './../components/Registration'
+import Navbar from './../components/Navbar'
 
 export default function WebTraderMain() {
     return (
-        <div className="bg-[#0089D31A] pt-20 pb-40 px-8">
-            <div className="mx-auto max-w-[1200px] w-full flex items-center flex-col lg:flex-row justify-between">
-                <div className="w-full mr-6 lg:w-1/2">
-                    <h1 className="font-extrabold text-xl md:text-6xl">Our
-                        Webtrader <span className='text-accent'> Platform</span></h1>
-                    <p className="font-medium mt-4 text-base">No name 1 is providing a new web based trading platform that runs on both PC and Mac. This platform, broadens our reach and addresses some ongoing needs of our clients who require a stable and flexible trading solution to access global forex markets. Our webtrader has been built with one purpose in mind and that's to improve the trading experience of our 
-clients. </p>
-                    <button className="mt-6 py-4 px-8 text-white font-medium bg-accent">TRY IT NOW</button>
+        <div className="bg-[#0089D31A] px-8">
+            <div className='mx-auto max-w-[1200px] '>
+                <div className="py-[50px]">
+                    <Registration color={'text-color-text'} />
                 </div>
-                <div>
-                    <img alt="laptop" className='mt-4' src={laptop} />
+                <Navbar navColor={'text-color-text'} />
+
+                <div className="w-full  pt-20 pb-40 flex items-center flex-col lg:flex-row justify-between">
+                    <div className="w-full mr-6 lg:w-1/2">
+                        <h1 className="font-extrabold text-xl md:text-6xl">Our
+                            Webtrader <span className='text-accent'> Platform</span></h1>
+                        <p className="font-medium mt-4 text-base">No name 1 is providing a new web based trading platform that runs on both PC and Mac. This platform, broadens our reach and addresses some ongoing needs of our clients who require a stable and flexible trading solution to access global forex markets. Our webtrader has been built with one purpose in mind and that's to improve the trading experience of our
+                            clients. </p>
+                        <button className="mt-6 py-4 px-8 text-white font-medium bg-accent">TRY IT NOW</button>
+                    </div>
+                    <div>
+                        <img alt="laptop" className='mt-4' src={laptop} />
+                    </div>
                 </div>
             </div>
         </div>
